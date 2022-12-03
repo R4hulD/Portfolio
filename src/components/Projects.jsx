0ef62158e5b527325a449cceb778bdd416c48ac2
R4hulD <rhl.dhwn.2002@gmail.com>
2022-12-03 16:18:08 +0530

    edit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,9 @@ const Projects = () => {
         <div>
           <ul className='list-disc'>
             <li>
-              <a href='https://spotter1.netlify.app' target="_blank" rel="noopener noreferrer" className='text-[#0a9396] text-2xl hover:text-[#e9d8a6]'>Spotter</a>
+              <a href='https://spotter1.netlify.app' target="_blank" rel="noopener noreferrer" className='text-[#0a9396] text-2xl hover:text-[#e9d8a6]'>Spotter
+              <br></br>
+              - A healthcare platform to take care of all the fitness and hygiene related needs at the click of a button, helping users by "spotting" them.</a>
             </li>
           </ul>
         </div>
